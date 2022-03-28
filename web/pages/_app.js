3e91e9cda7856c57a32c0778242af554ceca16fc
jeremel <jerlentz@gmail.com>
2022-03-28 11:19:11 -0400

    Update the layout import in _app.js to use the correct capital L in the import path. Hoping this will let the site build with Vercel now

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Sidebar from "../components/Sidebar";
 import styled from "styled-components";
 
