6c056a9be63d5c560b3375b1be3c24ac94a0b21c
jeremel <jerlentz@gmail.com>
2022-05-15 15:48:26 -0400

    Add a res.end() method to the exit-preview api

--- web/pages/api/exit-preview.js
+++ web/pages/api/exit-preview.js
@@ -3,5 +3,7 @@
 export default function exit(req, res) {
   res.clearPreviewData();
 
-  res.writeHead(307, { Location: req?.query?.slug ?? `/` });
+  // res.writeHead(307, { Location: req?.query?.slug ?? `/` });
+  res.writeHead(307, { Location: `/` });
+  res.end();
 }
