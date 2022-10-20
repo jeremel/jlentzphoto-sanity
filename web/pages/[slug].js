b30802ec48069cd088cd25b148cda1e613520351
jeremel <jerlentz@gmail.com>
2022-10-20 17:42:46 -0400

    Updated the image sizing on the photography pages so that the images fit on the screen better. You can now see the entire image without needing to scroll. I also updated the mark for the 'link' type in Sanity's portable text field so that target blank is added and rel = noreferrer is added when links to external sources are used.

--- web/pages/[slug].js
+++ web/pages/[slug].js
@@ -61,11 +61,9 @@ const Header = styled.header`
 `;
 
 const Content = styled.section`
-  /* width: 80vw; */
   width: 100%;
   background: white;
   padding: clamp(1.5rem, 1.027rem + 1.0811vw, 2rem);
-  /* padding-top: 1rem; */
   position: relative;
   z-index: 1;
   background: white;
@@ -75,7 +73,6 @@ const Content = styled.section`
   align-items: center;
 
   h2 {
-    /* transform: translateY(-80px); */
     background: white;
     margin: 0;
     padding: 0 1.5rem 1.5rem;
@@ -87,12 +84,16 @@ const Content = styled.section`
   }
 
   p {
-    /* margin: -4rem 0 1.25rem 0; */
     width: 60vw;
     font-size: clamp(1.4rem, 1.3333rem + 0.2963vw, 1.6rem);
     padding: 0;
-    /* text-align: center; */
-    text-indent: 2rem;
+  }
+
+  a {
+    text-decoration: underline;
+    text-decoration-style: dotted;
+    text-decoration-thickness: 3px;
+    text-underline-offset: 4px;
   }
 
   @media (max-width: 700px) {
@@ -106,11 +107,10 @@ const Content = styled.section`
 const Gallery = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 0.25rem;
+  gap: 0.5rem;
   margin: 0 2rem;
   justify-content: center;
   align-items: center;
-  /* max-width: 75vw; */
   width: 100%;
   position: relative;
   z-index: 1;
@@ -122,6 +122,8 @@ const Gallery = styled.div`
     overflow: hidden;
     border: 2px solid transparent;
     transition: border 0.25s ease;
+    max-width: 90vw;
+    max-height: 98vh;
 
     &: hover {
       border: 2px solid black;
@@ -129,7 +131,7 @@ const Gallery = styled.div`
 
     img {
       height: 100%;
-      max-width: 90vw;
+      width: 100%;
       transition: transform 1s ease;
 
       &:hover {
@@ -139,7 +141,7 @@ const Gallery = styled.div`
   }
 
   @media (max-width: 650px) {
-    img {
+    .imgContainer {
       max-width: 95vw;
     }
   }
@@ -219,6 +221,13 @@ const components = {
         </Link>
       );
     },
+    link: ({ value, children }) => {
+      return (
+        <a href={value.href} target="_blank" rel="noreferrer">
+          {children}
+        </a>
+      );
+    },
   },
 };
 
@@ -293,7 +302,7 @@ export default function Page({ data, preview }) {
   return (
     <Container>
       <Head>
-        {title && <title>{title} | Jereme Lentz Photography</title>}
+        {title && <title>{`${title} | Jereme Lentz Photography`}</title>}
         {description && <meta name="description" content={description} />}
         <link rel="icon" href="/jl-logo.png" />
       </Head>
